377678d285d14e2270ee25bf895832435416dbf9
Ming_tc51 <Ming_tc51@itstepru.onmicrosoft.com>
2021-11-21 22:17:31 +0500

    Добавьте файлы проекта.

---
+++ IntroductionToOOP/IntroductionToOOP.cpp
@@ -0,0 +1,10 @@
+﻿// IntroductionToOOP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    std::cout << "Hello World!\n";
+}
