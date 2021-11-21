193f911b5fdfa4f94d538e8e17813a05f919e1e7
Ming_tc51 <Ming_tc51@itstepru.onmicrosoft.com>
2021-11-22 00:21:03 +0500

    Yes

--- IntroductionToOOP/IntroductionToOOP.cpp
+++ IntroductionToOOP/IntroductionToOOP.cpp
@@ -2,9 +2,50 @@
 //
 
 #include <iostream>
+#define tab "\t"
+class Point
+{
+    double x;
+    double y;
+public:
+    //get методы должны быть константными
+    //константным называется метод, который не изменяет объект, для которого вызывается.
+    // Для константного объекта могут быть вызваны только константные методы.
+    double get_x()const
+    {
+        return x;
+   }
+    double get_y()const
+    {
+        return y;
+    }
 
+    void set_x(double x)
+    {
+       this->x = x;
+    }
+    void set_y(double y)
+    {
+        this->y = y;
+    }
+};
+
+//#define STRUCT_POINT
 int main()
 {
+    using namespace std;
     setlocale(LC_ALL, "Rus");
-    std::cout << "Hello World!\n";
+#ifdef STRUCT_POINT 
+    Point A;
+    A.x = 2;
+    A.y = 3;
+    cout << A.x << tab << A.y << endl;
+    Point* pA = &A;
+    cout << pA->x << tab << pA->y << endl;
+#endif STRUCT_POINT
+
+    Point A;
+    A.set_x(2);
+    A.set_y(3);
+    cout << A.get_x() << "\t" << A.get_y() << endl;
 }
--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
-
-int main()
-{
-    using namespace std;
-    setlocale(LC_ALL, "Rus");
-}
+//#include <iostream>
+//#define tab "\t"
+//struct Point
+//{
+//    double x;
+//    double y;
+//};
+//int main()
+//{
+//    using namespace std;
+//    setlocale(LC_ALL, "Rus");
+//    Point A;
+//    A.x = 2;
+//    A.y = 3;
+//    cout << A.x << tab << A.y << endl;
+//}
